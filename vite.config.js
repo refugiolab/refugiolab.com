b8ed2515e90ea90a8ab8488ee2d1169dd2fcc7b5
Refugio <somosrefugioficial@gmail.com>
2025-08-22 18:56:48 -0300

    fix: Corregir el error de MIME type y las rutas del build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Agrega esta línea para corregir las rutas de tus archivos.
-  // Esto soluciona el error 'MIME type' y los errores 404 en la carga de módulos.
-  base: './',
+  base: './', // Esta línea es la que soluciona el problema de las rutas.
 })
