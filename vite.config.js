a54fb7a0ebc418dc77f22e0a4a72425a83da9395
Refugio <somosrefugioficial@gmail.com>
2025-08-22 20:01:39 -0300

    version inicial nueva V

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ Set the base path to an empty string for correct relative paths.
-  base: '',
+  // ⚠️ Esta configuración es fundamental para el despliegue
+  base: '/',
 })
