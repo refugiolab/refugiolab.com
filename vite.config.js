646ff6dc5ef0e36e0d409d8cf2c41dfdf2f749a5
Refugio <somosrefugioficial@gmail.com>
2025-08-22 18:55:48 -0300

    fix: Corregir la ruta base para el despliegue en Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Agrega esta línea para corregir las rutas de tus archivos.
+  // Esto soluciona el error 'MIME type' y los errores 404 en la carga de módulos.
+  base: './',
 })
